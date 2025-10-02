b18baf28d4fc8cec59b5349bec601108784cc33c
Administrator Domeny <admin@pracownia.arpa>
2025-10-02 14:11:12 +0200

    mja wersaja v1

--- 34567834567/script.js
+++ 34567834567/script.js
@@ -11,31 +11,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // --- pasek postępu ---
   const pasek = document.getElementById('pasek');
-  let procent = 4;               // start jak w CSS
-  const KROK = 12;
+  let procent = 5;               // start jak w CSS
+  const KROK = 13;
+
   const ustaw = p => pasek.style.width = Math.min(100, Math.max(0, p)) + '%';
-  ustaw(procent);
+
+  ustaw(procent); 
 
   const inputs = document.querySelectorAll('.main input');
   inputs.forEach(el => {
-    // każde wyjście z pola zwiększa pasek o +12%
+    // każde wyjście z pola zwiększa pasek o +13%
     el.addEventListener('blur', () => {
       procent = Math.min(100, procent + KROK);
       ustaw(procent);
     });
   });
 
+ function zwiekszPostep() {
+    ustaw(procent +KROK)
+  }
+
   // --- zatwierdzenie ---
-  const confirmBtn = document.querySelector('#blok3 .confirm');
+  const confirmBtn = document.querySelector('#blok4 .confirm');
   confirmBtn?.addEventListener('click', () => {
-    const imie      = document.querySelectorAll('#blok1 input[type="text"]')[0]?.value || '';
-    const nazwisko  = document.querySelectorAll('#blok1 input[type="text"]')[1]?.value || '';
-    const dataUr    = document.querySelector('#blok1 input[type="date"]')?.value || '';
-    const ulica     = document.querySelectorAll('#blok2 input[type="text"]')[0]?.value || '';
-    const numer     = document.querySelector('#blok2 input[type="number"]')?.value || '';
-    const miasto    = document.querySelectorAll('#blok2 input[type="text"]')[1]?.value || '';
-    const telefon   = document.querySelector('#blok3 input[type="tel"]')?.value || '';
-    const rodo      = document.querySelector('#blok3 input[type="checkbox"]')?.checked || false;
+    const imie      = document.querySelectorAll('#blok2 input[type="text"]')[0]?.value || '';
+    const nazwisko  = document.querySelectorAll('#blok2 input[type="text"]')[1]?.value || '';
+    const dataUr    = document.querySelector('#blok2 input[type="date"]')?.value || '';
+    const ulica     = document.querySelectorAll('#blok3 input[type="text"]')[0]?.value || '';
+    const numer     = document.querySelector('#blok3 input[type="number"]')?.value || '';
+    const miasto    = document.querySelectorAll('#blok3 input[type="text"]')[1]?.value || '';
+    const telefon   = document.querySelector('#blok4 input[type="tel"]')?.value || '';
+    const rodo      = document.querySelector('#blok4 input[type="checkbox"]')?.checked || false;
 
     console.clear();
     console.log('--- Dane klienta ---');
